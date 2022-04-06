142e4178f8a58242f3e4e1b71ca9f7a01df97903
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-04-05 21:32:38 -0400

    added env files

--- src/main/java/world/ntdi/mathutils/JDA.java
+++ src/main/java/world/ntdi/mathutils/JDA.java
@@ -2,29 +2,25 @@ package world.ntdi.mathutils;
 
 import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import world.ntdi.mathutils.Commands.PhysicsCommands;
-import world.ntdi.mathutils.Commands.QuadraticCommands;
-import world.ntdi.mathutils.Commands.Slash.*;
+import world.ntdi.mathutils.Commands.Slash.Physics.*;
+import world.ntdi.mathutils.Commands.Slash.Quadratic.*;
 import world.ntdi.mathutils.Commands.UtilCommands;
 import world.ntdi.mathutils.Listener.JoinListener;
 import world.ntdi.mathutils.Listener.ReadyListener;
 
 import javax.security.auth.login.LoginException;
-import java.util.Objects;
 
 
 public class JDA {
     public static void main(String[] args) throws LoginException, InterruptedException {
         CommandClientBuilder builder = new CommandClientBuilder();
-        builder.addSlashCommands(new QuadForm(), new Discriminant(), new Qalc(), new FindAll(), new ExpoFunc());
+        builder.addSlashCommands(new QuadForm(), new Discriminant(), new Qalc(), new FindAll(), new ExpoFunc(), new Force(), new Work(), new Power(), new KE(), new GPE());
         builder.forceGuildOnly(959638142896439307L);
         builder.setServerInvite("https://discord.gg/VU7kWC9Nda");
         builder.setOwnerId("811580599068262421");
@@ -33,7 +29,8 @@ public class JDA {
         builder.setActivity(Activity.watching("math"));
         CommandClient commandClient = builder.build();
 
-        net.dv8tion.jda.api.JDA jda = JDABuilder.createDefault("OTU5NjM5OTY3MDg3MzQ1Njc0.Yke0XQ.icdz9tiglcwfmbATxhnkcKUT8S8")
+        Dotenv dotenv = Dotenv.configure().load();
+        net.dv8tion.jda.api.JDA jda = JDABuilder.createDefault(dotenv.get("TOKEN"))
                 .disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE)
                 .setBulkDeleteSplittingEnabled(false)
                 .addEventListeners(
